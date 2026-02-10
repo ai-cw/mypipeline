80e705f8eb170fa0388d142cfb2b40bec96ac15e
aismail <aismail@chartwell.com>
2026-02-10 18:46:50 -0500

    Add email notification message to script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "The pipeline works successfully! This is an update to the script.sh file.This is a second update to the script.sh file after failure"
+
+echo "sending an email to the user to notify that the pipeline works successfully"
