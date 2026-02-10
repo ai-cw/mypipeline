1e190f0397b9245a0244499fd47e09fad373054f
aismail <aismail@chartwell.com>
2026-02-10 18:59:48 -0500

    Fix formatting in script.sh by adding missing newline at the end of the file

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@
 echo "The pipeline works successfully! This is an update to the script.sh file.This is a second update to the script.sh file after failure"
 
 echo "sending an email to the user to notify that the pipeline works successfully!!"
+
+echo "This is a seventh update to the script.sh file after failure"
